777dc1f44da663fcc1dcbb1c7492283921db78ce
Michael Chapman <submariner100@gmail.com>
2016-10-07 17:46:23 +0100

    This is the first commit after creating the UI interface. This was done using stack views.

---
+++ RockMyBoots/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RockMyBoots
+//
+//  Created by Macbook on 06/10/2016.
+//  Copyright © 2016 Chappy-App. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    
+  }
+
+  
+}
+
